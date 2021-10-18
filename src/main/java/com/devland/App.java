24a838160ae00ba196d2fe26039291539db0ed92
Naufal Muhammad Farras <nmfarras@gmail.com>
2021-10-18 20:18:37 +0700

    intial commit

---
+++ src/main/java/com/devland/App.java
@@ -0,0 +1,18 @@
+package com.devland;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+    }
+}
